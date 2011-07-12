fa6ebffe39cd6ea3c93384ace5413da9d79c7e2d
apatterson <a.patterson@ucc.ie>
2011-07-12 22:13:27 +0100

    image map

--- src/hello.clj
+++ src/hello.clj
@@ -1,6 +1,6 @@
 (ns hello-world
     (:use compojure.core, ring.adapter.jetty, incanter.core, incanter.stats, incanter.charts,
-	  incanter.io ,hiccup.core)
+	  incanter.io ,hiccup.core, hiccup.page-helpers)
     (:require [compojure.route :as route] 
 	      [clojure.contrib.string :as str] 
 	      [clojure.contrib.math])
@@ -21,48 +21,57 @@
 		       (.addln gv 
 			       (str nodename "->" 
 				    (nth head (+ index 1)) 
-				    "[ headlabel =\"" (* weight 4) 
-				    "\", weight=" (* (abs weight) 4) 
-				    ", color=" colour 
-				    " labeldistance=3];")))))))
+				    "[headlabel =\"" (* weight 4) 
+				    "\",weight=" (* (abs weight) 4) 
+				    ",color=" colour 
+				    "];")))))))
 
 
 
-;; Pass a map as the first argument to be 
-;; set as attributes of the element
-(defn html-doc 
-  [title & body] 
-  (html 
-       [:div 
-	[:h2 
-	 [:a {:href "/"} 
-          "Generate a normal sample"]]]
-       body)) 
-
-(defn graph-viz
+(defn do-graph
   [id]
+  (def gv (GraphViz.))
   (with-open [fr (java.io.FileReader. "data/Mike - FCM Cork Harbour.csv")
 	     br (java.io.BufferedReader. fr)]
 	     (let 	     	
 		 [ls (line-seq br)
-		 head (str/split #"\t" (first ls))
-		 gv (new GraphViz)]
+		 head (str/split #"\t" (first ls))]
 		 (.addln gv (.start_graph gv))
 		 (doseq [nodename (next head)] 
-			( #(.addln gv (str nodename "[shape=box];"))))
+			( #(.addln gv (str nodename "[shape=box,URL=" nodename "];"))))
 		 (doseq [line (next ls)] (to-dot line head gv))
-		 (.addln gv (.end_graph gv))
-		 (let [graph (.getGraph gv (.getDotSource gv) "gif")
-		      in-stream (do
-				    (new ByteArrayInputStream graph))]
-				    {:status 200	 
-				    :headers {"Content-Type" "image/gif"}
-				    :body in-stream}))))
+		 (.addln gv (.end_graph gv)))))
+
+(defn do-map
+  [id]
+  (do-graph id)
+  (let [graph (.getGraph gv (.getDotSource gv) "cmapx")]
+       (String. graph)))
+
+(defn graph-viz
+  [id]
+  (do-graph id)
+  (let [graph (.getGraph gv (.getDotSource gv) "gif")
+       in-stream (do
+		     (ByteArrayInputStream. graph))]
+		     {:status 200	 
+		     :headers {"Content-Type" "image/gif"}
+		     :body in-stream}))
   
+(defn html-doc 
+  [id] 
+  (html4
+   (do-map id)
+   [:div 
+   "<IMG SRC=\"/img/xx\" ismap usemap=\"#G\" />"]
+   )) 
+
   
   ;; define routes
 (defroutes webservice
-  (GET "/:doi" [id] (graph-viz id) ))
+  (GET "/:id" [id] (html-doc id) )
+  (GET "/img/:id" [id] (graph-viz id) )
+  (GET "/map.map" [id] (do-map id) ))
 
-(run-jetty webservice {:port 8080})
+(run-jetty webservice {:port 8000})
   
