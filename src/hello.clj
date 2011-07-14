8c69cc2007a07acd7cbc958d41b89b6a7a8b88e3
Anthony Patterson <a.patterson@ucc.ie>
2011-07-14 15:16:31 +0100

    hyperlinks and highlighting
    Signed-off-by: Anthony Patterson <a.patterson@ucc.ie>

--- src/hello.clj
+++ src/hello.clj
@@ -7,27 +7,39 @@
     (:import (java.io ByteArrayOutputStream
 		      ByteArrayInputStream)
 	     GraphViz
-	     (java.net URLEncoder)))
+	     (java.net URLEncoder
+		       URLDecoder)))
 
 
+(defn encode-nodename
+  [nodename]
+  (URLEncoder/encode (str/replace-re #"[/\":,]" "" nodename)))
 
-(defn to-dot [line head gv]
+(defn to-dot [line head id gv]
   (let [linevec (str/split #"\t" line)
        nodename (first linevec)
        idv (map vector (iterate inc 0) (next linevec))]
        (doseq [[index value] idv]
 	      (let [weight (Float/parseFloat value)
-		   colour (if (> weight 0) "blue" "red")]
-		   (if (not= weight 0.0)
-		       (.addln gv 
-			       (str nodename "->" 
-				    (nth head (+ index 1)) 
-				    "[headlabel =\"" (* weight 4) 
-				    "\",weight=" (* (abs weight) 4) 
-				    ",color=" colour 
-				    "];")))))))
+		   colour (if (or (not id) (= (encode-nodename nodename) 
+					      (encode-nodename id)))
+			      (if (> weight 0) "blue" "red")
+			    "grey")]
+			    (if (not= weight 0.0)
+				(.addln gv 
+					(str nodename "->" 
+					     (nth head (+ index 1)) 
+					     "[headlabel =\"" (* weight 4) 
+					     "\",weight=" (* (abs weight) 4) 
+					     ",color=" colour 
+					     "];")))))))
 
-
+(defn print-node
+  [nodename id gv]
+  (let [url (encode-nodename nodename)
+       fillcolour (if id 
+		      (if (= url (URLEncoder/encode id)) "blue" "gray81") "blue")]
+		      (.addln gv (str nodename " [shape=box,URL=\"/node/" url "\" color=" fillcolour ",style=filled];"))))
 
 (defn do-graph
   [id]
@@ -38,9 +50,8 @@
 		 [ls (line-seq br)
 		 head (str/split #"\t" (first ls))]
 		 (.addln gv (.start_graph gv))
-		 (doseq [nodename (next head)] 
-			( #(.addln gv (str nodename "[shape=box,URL=" nodename "];"))))
-		 (doseq [line (next ls)] (to-dot line head gv))
+		 (doseq [nodename (next head)] (print-node nodename id gv))
+		 (doseq [line (next ls)] (to-dot line head id gv))
 		 (.addln gv (.end_graph gv)))))
 
 (defn do-map
@@ -64,14 +75,17 @@
   (html4
    (do-map id)
    [:div 
-   "<IMG SRC=\"/img/xx\" ismap usemap=\"#G\" />"]
+   (str "<IMG SRC=\"/img/" id "\" border=\"0\" ismap usemap=\"#G\" />")]
    )) 
 
   
   ;; define routes
 (defroutes webservice
   (GET "/:id" [id] (html-doc id) )
+  (GET "/" [] (html-doc nil) )
   (GET "/img/:id" [id] (graph-viz id) )
+  (GET "/img/" [] (graph-viz nil) )
+  (GET "/node/:id" [id] (html-doc id) )
   (GET "/map.map" [id] (do-map id) ))
 
 (run-jetty webservice {:port 8000})
