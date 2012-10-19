30934c5847855b19697427785c535b98d6c32200
Anthony Patterson <a.patterson@ucc.ie>
2012-10-19 13:31:08 +0200

    cleaning up code and depndencies

--- project.clj
+++ project.clj
@@ -2,11 +2,9 @@
   :description "iasess prototype"
   :dependencies [[org.clojure/clojure "1.3.0"]
 		 [compojure "1.1.3"]
-                 [org.clojars.pallix/analemma "1.0.0"]
 		 [ring "1.1.6"]
 		 [hiccup "1.0.1"]
                  [com.ashafa/clutch "0.3.1-SNAPSHOT"]
                  [dorothy "0.0.3"]]
-  :java-source-path  "java"
   :ring {:handler hello}
   :main hello)
--- src/hello.clj
+++ src/hello.clj
@@ -6,11 +6,9 @@
             [clojure.xml :as xml] 
             [clojure.contrib.string :as str] 
             [clojure.contrib.math :as math]
-            [com.ashafa.clutch :as clutch]
-            [analemma.xml :as ana])
+            [com.ashafa.clutch :as clutch])
   (:import (java.io ByteArrayOutputStream
                     ByteArrayInputStream)
-           GraphViz
            (java.net URLEncoder
                      URLDecoder
                      URL)))
@@ -31,157 +29,6 @@
 (defn encode-nodename
   [nodename]
   (URLEncoder/encode (str/replace-re #"[^a-zA-Z0-9]" "" nodename)))
-
-(defn node-string
-  [nodename strength url dir format data-file fillcolour]
-   (str nodename " [shape=box,URL=\"/resilience/strength/"
-        strength "/node/" url "/dir/" dir "/format/" format "/data/" data-file "\",color="
-        fillcolour ",style=filled];"))
-   
-(defn to-dot [line head id strength dir gv]
-  (let [linevec (str/split #"\t" line)
-        nodename (first linevec)
-        idv (map vector (iterate inc 0) (next linevec))]
-    (doseq [[index value] idv]
-      (let [weight (Float/parseFloat value)
-            arrowhead (nth head (+ index 1))
-            comparees  (list nodename arrowhead)
-            comparee (if (= dir "in") (remove #{nodename} comparees)
-                         (if (= dir "out") (remove #{arrowhead} comparees)
-                             comparees))
-            min-weight (if (= strength "strong") 
-                         0.6 
-                         (if (= strength "medium") 0.3 0.0))
-            highlighted (some #(or (= id "all")
-                                  (= (encode-nodename %1) 
-                                     (encode-nodename id))) comparee)
-            weight-factor 10
-            colour (if highlighted (if (> weight 0) positive negative)
-                       lowlight)]
-        (if (> (math/abs weight) min-weight) 
-          (.addln gv 
-                  (str nodename "->" 
-                       (nth head (+ index 1)) 
-                       "[headlabel =\"" (* weight 4) 
-                       "\",weight=" (* (math/abs weight) weight-factor) 
-                       ",color=" colour 
-                       "];")))))))
-
-(defn dot-from-lifemap [line id strength dir data-file format gv]
-  (let [linevec (str/split #"," line)
-        nodename (first linevec)
-        url (encode-nodename nodename)
-        arrowhead (nth linevec 2)
-        fillcolour (if (not= "all" id) 
-                     (if (= url (URLEncoder/encode id)) highlight lowlight) highlight)
-        arrowhead-fillcolour (if (not= "all" id) 
-                     (if (= (encode-nodename arrowhead) (URLEncoder/encode id)) highlight lowlight) highlight)
-        weight ((keyword (nth linevec 1)) strengths)
-        comparees  (list nodename arrowhead)
-        comparee (if (= dir "in") (remove #{nodename} comparees)
-                     (if (= dir "out") (remove #{arrowhead} comparees)
-                         comparees))
-        min-weight (if (= strength "strong") 
-                     0.6 
-                     (if (= strength "medium") 0.3 0.0))
-        highlighted (some #(or (= id "all")
-                               (= (encode-nodename %1) 
-                                  (encode-nodename id))) comparee)
-        weight-factor 10
-        colour (if highlighted (if (> weight 0) positive negative)
-                   lowlight)
-        dot-string (str "\"" nodename "\" -> " 
-                        "\"" arrowhead "\""
-                        " [headlabel =\"" (* weight 4) 
-                        "\",weight=" (* (math/abs weight) weight-factor) 
-                        ",color=" colour 
-                        "];")
-        nodename-string (node-string (str "\"" nodename "\"") strength url dir format data-file fillcolour)
-        arrowhead-string (node-string (str "\"" arrowhead "\"") strength (encode-nodename arrowhead) dir format data-file arrowhead-fillcolour)]
-    (.addln gv arrowhead-string)
-    (.addln gv nodename-string) 
-    (if (> (math/abs weight) min-weight) 
-      (.addln gv dot-string))))
-
-(defn print-node
-  [nodename id strength dir data-file format gv]
-  (let [url (encode-nodename nodename)
-        fillcolour (if (not= "all" id) 
-                     (if (= url (URLEncoder/encode id)) highlight lowlight) highlight)]
-    (.addln gv (node-string nodename strength url dir format data-file fillcolour ))))
-
-(defn do-graph
-  [id strength dir data-file format]
-  (def gv (GraphViz.))
-  (with-open [fr (if (re-find #"^http:" data-file)
-                   (java.io.InputStreamReader. (.openStream (URL. data-file)))
-                   (java.io.FileReader. (str "data/" (URLDecoder/decode data-file))))
-              br (java.io.BufferedReader. fr)]
-    (if (= format "LifeMap")
-      (let 	     	
-          [ls (line-seq br)]
-        (.addln gv (.start_graph gv))
-        (doseq [line ls] (dot-from-lifemap line id strength dir data-file format gv))
-        (.addln gv (.end_graph gv))
-        #_(println (.getDotSource gv)))
-      (let 	     	
-          [ls (line-seq br)
-           head (str/split #"\t" (first ls))]
-        (.addln gv (.start_graph gv))
-        (doseq [nodename (next head)] (print-node nodename id strength dir data-file format gv))
-        (doseq [line (next ls)] (to-dot line head id strength dir gv))
-        (.addln gv (.end_graph gv))))))
-
-(defn do-map
-  [id strength dir data-file format]
-  (do-graph id strength dir data-file format)
-  (let [graph (.getGraph gv (.getDotSource gv) "cmapx")]
-    (String. graph)))
-
-(defn graph-viz
-  ([id strength dir data-file format]
-     (graph-viz id strength dir "no" data-file format))
-  ([id strength dir link data-file format]
-     (do-graph id strength dir data-file format)
-     (let [graph (.getGraph gv (.getDotSource gv) "svg")
-           in-stream (do
-                       (ByteArrayInputStream. graph))]
-       {:status 200	 
-        :headers {"Content-Type" "image/svg+xml"}
-        :body in-stream})))
-
-(defn html-doc 
-  ([id strength dir data-file format]
-     (html-doc id strength dir "no" data-file format))
-  ([id strength dir link data-file format]
-     (xhtml
-      (do-map id strength dir data-file format)
-      [:div {:style "float: left;width: 400px"}
-       (form-to [:get "/resilience/new"]
-                (text-field "URL" data-file)
-                (text-field "Format" format)
-                (submit-button "Submit"))]
-      [:div {:style "float: left;width: 200px"}
-       [:ul
-        (doall (map #(xhtml [:li (if (= strength %)
-                                   %
-                                   [:a {:href (str "/resilience/strength/" % "/node/" id "/dir/" dir "/format/" format "/data/" data-file)} %])])
-                    ["weak" "medium" "strong"]))]]
-      (if (not= id "all")
-        [:div
-         [:div {:style "float: left;width: 200px"}
-          [:ul
-           (doall
-            (map
-             #(xhtml [:li
-                      (if(= dir %)
-                        %
-                        [:a {:href (str "/resilience/strength/" strength "/node/" id "/dir/" % "/format/" format "/data/" data-file)} %])])
-             ["in" "out" "inout"]))]]
-         [:div {:style "float: left;width: 200px"}
-          [:ul [:li [:a {:href (str "/resilience/strength/" strength "/node/all/dir/inout/format/" format "/data/" data-file)} "all"]]]]])
-      [:div {:style "clear: both"}
-       (str "<IMG SRC=\"/resilience/strength/" strength "/img/" id "/dir/" dir "/format/" format "/data/" data-file "\" border=\"0\" ismap usemap=\"#G\" />")])))
 
 (def drivers       ["Environmental Legislation and Policy"
                     "Tourism and Recreation"
@@ -432,28 +279,6 @@
   (GET "/resilience/:id/mode/:mode/:tail/:node/:weight" {params :params} (edit-links-html params))
   (GET "/resilience/test" {params :params} (edit-links-html {"mode" "edit"}))
   
-  (GET ["/resilience/strength/:strength/node/:id/dir/:dir/new/:link/format/:format/data/:data-file" :data-file #".*$"] [id strength dir link data-file format] (html-doc id strength dir link data-file format) )
-  (GET ["/resilience/strength/:strength/node/:id/dir/:dir/format/:format/data/:data-file" :data-file #".*$"] [id strength dir link data-file format] (html-doc id strength dir data-file format) )
-  (GET ["/resilience/strength/:strength/node/:id/dir/:dir/data/:data-file" :data-file #".*$"] [id strength dir data-file] (html-doc id strength dir data-file default-format) )
-  (GET "/resilience/strength/:strength/node/:id/dir/:dir" [id strength dir] (html-doc id strength dir default-data-file default-format) )
-  (GET "/resilience/strength/:strength/node/:id" [id strength] (html-doc id strength "out" default-data-file default-format) )
-  (GET "/resilience/strength/:strength/node/" [strength] (html-doc "all" strength "out" default-data-file default-format) )
-  (GET "/resilience/strength/:strength" [strength] (html-doc "all" strength "out" default-data-file default-format) )
-  (GET "/resilience/node/:id" [id] (html-doc id "weak" "out" default-data-file default-format) )
-  (GET "/resilience/" [] (html-doc "all" "weak" "out" default-data-file default-format) )
-  (GET ["/resilience/data/:data-file" :data-file #".*$"] [data-file] (html-doc "all" "weak" "out" data-file default-format) )
-  (GET "/resilience/" [] (html-doc "all" "weak" "out" default-data-file default-format) )
-  ;;probably don't need half of these
-  (GET ["/resilience/strength/:strength/img/:id/dir/:dir/format/:format/data/:data-file" :data-file #".*$"] [id strength dir data-file format] (graph-viz id strength dir data-file format) )
-  (GET ["/resilience/strength/:strength/img/:id/dir/:dir/data/:data-file" :data-file #".*$"] [id strength dir link data-file] (graph-viz id strength dir link data-file default-format) )
-  (GET ["/resilience/strength/:strength/img/:id/dir/:dir/new/:link/format/:format/data/:data-file" :data-file #".*$"] [id strength dir link data-file format] (graph-viz id strength dir link data-file format) )
-  (GET ["/resilience/strength/:strength/img/:id/dir/:dir/new/:linkdata/:data-file" :data-file #".*$"] [id strength dir data-file] (graph-viz id strength dir data-file default-format) )
-  (GET "/resilience/strength/:strength/img/:id/dir/:dir" [id strength dir] (graph-viz id strength dir default-data-file default-format) )
-  (GET "/resilience/strength/:strength/img/:id" [id strength] (graph-viz id strength "out" default-data-file default-format) )
-  (GET "/resilience/strength/:strength/img/" [strength] (graph-viz "all" strength "out" default-data-file default-format) )
-  (GET "/resilience/img/:id" [id] (graph-viz id "weak" "out") )
-  (GET "/resilience/img/" [] (graph-viz "all" "weak" "out") )
-  (GET "/resilience/new" {params :params} (html-doc "all" "weak" "out" (params "URL") (params "Format")))
   (resources "/"))
 
 
