4041cf2c01a6fcdafd8ef8273dbd7696f9b7cbad
Anthony Patterson <a.patterson@ucc.ie>
2012-11-09 15:51:13 +0000

    ISS-14 incanter

--- project.clj
+++ project.clj
@@ -5,6 +5,8 @@
 		 [ring "1.1.6"]
 		 [hiccup "1.0.1"]
                  [com.ashafa/clutch "0.3.1-SNAPSHOT"]
-                 [dorothy "0.0.3"]]
+                 [dorothy "0.0.3"]
+                 [dk.ative/docjure "1.6.0-SNAPSHOT"]
+                 [incanter "1.3.0"]]
   :ring {:handler hello}
   :main hello)
