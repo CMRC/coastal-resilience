1ed630fa3c63259ff1ac7d9276a58a18fc108144
Anthony Patterson <a.patterson@ucc.ie>
2012-11-13 12:20:55 +0000

    ISS-14 svg

--- project.clj
+++ project.clj
@@ -7,6 +7,7 @@
                  [com.ashafa/clutch "0.3.1-SNAPSHOT"]
                  [dorothy "0.0.3"]
                  [dk.ative/docjure "1.6.0-SNAPSHOT"]
-                 [incanter "1.3.0"]]
+                 [incanter "1.3.0"]
+                 [org.clojars.pallix/batik "1.7.0"]]
   :ring {:handler hello}
   :main hello)
