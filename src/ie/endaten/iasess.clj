aa0cf8105deefbefe9d94ab2cadadb6a09a17e9a
Anthony Patterson <a.patterson@ucc.ie>
2013-02-05 20:41:19 +0000

    tweaking styles

--- src/ie/endaten/iasess.clj
+++ src/ie/endaten/iasess.clj
@@ -465,8 +465,8 @@
           [:title "Iasess - Ireland's Adaptive Social-Ecological Systems Simulator"]
           [:script {:type "text/javascript"}
            "var dojoConfig = { parseOnLoad: true };"]
-          [:script {:src "http://serverapi.arcgisonline.com/jsapi/arcgis/3.3compact"}]
-          [:script {:src "/iasess/js/esri.js"}]
+          #_[:script {:src "http://serverapi.arcgisonline.com/jsapi/arcgis/3.3compact"}]
+          #_[:script {:src "/iasess/js/esri.js"}]
           [:script {:src "/iasess/js/script.js"}]
           [:style {:type "text/css"} "@import \"http://serverapi.arcgisonline.com/jsapi/arcgis/3.3/js/esri/css/esri.css\";"]
           [:style {:type "text/css"} "@import \"/iasess/css/layout.css\";"]
