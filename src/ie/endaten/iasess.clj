d73896ca383f91ce6aa5f904a0ae369409f865f2
Anthony Patterson <a.patterson@ucc.ie>
2013-02-05 14:35:21 +0000

    moving to main map

--- src/ie/endaten/iasess.clj
+++ src/ie/endaten/iasess.clj
@@ -527,11 +527,11 @@
            [:div {:id "graph"}
             (edit-links (assoc-in params [:format] "img") nodes links concepts)]
            #_[:iframe {:id "map" :height "100%" :frameborder "0" :scrolling "no" :marginheight "0"
-                     :marginwidth "0" :src
-                     "http://www.arcgis.com/home/webmap/embedViewer.html?webmap=f865f4eeb9fa473485962d5d60613cba&amp;extent=-12.7473,51.7862,-3.9088,55.1142"}]
-	    [:div {:class "container_12"}
-              [:div {:class "grid_12 rounded" :id "mapSection"}
-                [:div {:id "mainMap"}]]]
+                       :marginwidth "0" :src
+                       "http://www.arcgis.com/home/webmap/embedViewer.html?webmap=f865f4eeb9fa473485962d5d60613cba&amp;extent=-12.7473,51.7862,-3.9088,55.1142"}]
+           [:div {:class "container_12"}
+            [:div {:class "grid_12 rounded" :id "mapSection"}
+             [:div {:id "mainMap"}]]]
            [:div {:id "bar"}
             [:div {:id "info-text"} "Information panel: Mouse over Menu, Mapping Panel, or Modelling Panel to begin."]
             (edit-links-html (assoc-in params [:mode] "bar"))
